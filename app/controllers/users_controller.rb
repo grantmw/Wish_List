371130f7b4e02afdab7002aa906eb3a97ccdb035
Kendall <kendallwong1@gmail.com>
2016-07-12 22:18:01 -0400

    Very close for users to create their own profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,27 @@ class UsersController < ApplicationController
 	end
 
 	def show
+		p "****************" * 10
 		p params
 		@user=User.find(params["id"])
 		@wishlists=@user.wishlists
-
 		p @user
+		p "****************" * 10
 	end
+
+	def new
+
+  	end
+ 
+  	def create
+
+  		@user=User.new(user_name: params["username"], password: params["password"])
+  		p "****************" * 10
+  		p params["username"]
+  		p "****************" * 10
+  		@user.save
+  		redirect_to @user
+
+  	end
 
 end
